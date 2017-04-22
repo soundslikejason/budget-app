a1a173440e754cf6c2c67a434fe5d5f65ed6d78f
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-22 15:20:21 -0700

    finishing touches: improving the UX

--- app.js
+++ app.js
@@ -211,6 +211,12 @@ var UIController = (function() {
     
   };
   
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+  
   return {
     getInput: function() {
       return {
@@ -285,13 +291,6 @@ var UIController = (function() {
       
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
       
-      
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-      
       nodeListForEach(fields, function(current, index) {
         
         if (percentages[index] > 0) {
@@ -321,6 +320,21 @@ var UIController = (function() {
       // document.querySelector(DOMstrings.dateLabel).textContent = month + ' ' + year;
     },
     
+    changedType: function() {
+      
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue);
+      
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+      
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+      
+    },
+    
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -346,6 +360,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
     
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+    
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     
   };
   
